ea36e77b482d062686486dcea08cd413cbf064c5
Porco <porcorossoj89@gmail.com>
2025-07-29 20:13:30 +0400

    tested example app on testnet

--- src/example.tsx
+++ src/example.tsx
@@ -31,7 +31,7 @@ class Token extends Contract.fromAztec(TokenContract as any) {}
 
 //const NODE_URL = "http://localhost:8080"
 const NODE_URL = "https://aztec-alpha-testnet-fullnode.zkv.xyz"
-//const WALLET_URL = "http://localhost:5173"
+// const WALLET_URL = "http://localhost:5173"
 const WALLET_URL = "https://app.obsidion.xyz"
 
 const sdk = new AztecWalletSdk({
